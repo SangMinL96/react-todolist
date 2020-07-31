cbd9a1b7a5a9c20dc3f8b690f2edd239889f3360
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-31 21:12:24 +0900

    Updates

---
+++ precache-manifest.8e5f4ad2f380e62eaa77c478b117995b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c342d753a331aa2bf825673b386a66a",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "7a40f4a264e6be71c679",
+    "url": "/react-todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8606134ad71a79e8b387",
+    "url": "/react-todolist/static/js/2.54152962.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist/static/js/2.54152962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a40f4a264e6be71c679",
+    "url": "/react-todolist/static/js/main.2622494e.chunk.js"
+  },
+  {
+    "revision": "bdb1eb0d6159838c9f4b",
+    "url": "/react-todolist/static/js/runtime-main.6228bffe.js"
+  }
+]);
